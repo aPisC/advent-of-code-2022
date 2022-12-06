c4ac00bcbdbd2fa3f1dedea1a7a36546c8a9dd77
Bendeguz Cseresnyes <bendeguz.cseresnyes@hiya.com>
2022-12-06 17:02:45 +0100

    [Day 4] Refactor

--- aoc4/src/main/scala/example/Main.scala
+++ aoc4/src/main/scala/example/Main.scala
@@ -2,41 +2,39 @@ package example
 
 import scala.io.Source
 import java.util.Random
+import java.util.IllegalFormatException
 
 case class Range(start: Int, end: Int) {
-  def contains(other: Range) = start <= other.start && end >= other.end
+  def contains(other: Range) =
+    start <= other.start && end >= other.end
+
+  def overlaps(other: Range) =
+    (this contains other) || (other contains this)
+
   def intersects(other: Range) =
     contains(other) ||
       other.contains(this) ||
       (start until (end + 1) contains other.start) ||
       (start until (end + 1) contains other.end)
-
 }
 
 object Main extends App {
   val path = "inputs/input"
   val file = Source.fromFile(path)
+
   val result = file.getLines
-    .map(l => {
-      val sl = l.split(',')
-      val r1 = Range(sl(0).split('-')(0).toInt, sl(0).split('-')(1).toInt)
-      val r2 = Range(sl(1).split('-')(0).toInt, sl(1).split('-')(1).toInt)
-      (r1, r2)
-    })
-
-    // // For task 1
-    // .map {
-    //   case (r1, r2) if r1.contains(r2) => 1
-    //   case (r1, r2) if r2.contains(r1) => 1
-    //   case _                           => 0
-    // }
-
-    // For task 2
     .map {
-      case (r1, r2) if r1.intersects(r2) => 1
-      case _                             => 0
+      case s"$a-$b,$c-$d" =>
+        Some((Range(a.toInt, b.toInt), Range(c.toInt, d.toInt)))
+      case _ => None
+    }
+    .map {
+      // case Some((r1, r2)) if r1 intersects r2  => 1
+      case Some((r1, r2)) if r1 overlaps r2 => 1
+      case _                                => 0
     }
     .sum
 
+  file.close()
   println(result)
 }
